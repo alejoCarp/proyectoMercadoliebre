921cde84356565d65e0710b7b8de33c2b67da4d6
alejoCarp <alejocorvalan@yahoo.com.ar>
2022-12-14 21:58:49 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+let publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.listen(3000, () =>{console.log("RUNNING SERVER 3000")});
+
+app.get("/", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/register", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
